eda235b768c0caf8a6e29b6b1432b641fad7d3ca
roman1117459II <roman1117459@gmail.com>
2023-11-19 21:32:06 +0600

    changing new feature added

--- 0app.js
+++ 0app.js
@@ -1,1 +1,8 @@
 console.log("Hello JS Starting By Zaima!")
+console.log("Hello JS Starting By Ratul!")
+
+var gg= ["zaima", "ratul", "rtza"]
+
+for(var i= 0; i<gg.len(); i++){
+	console.log(gg[i]);
+}
