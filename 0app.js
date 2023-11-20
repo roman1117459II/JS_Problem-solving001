3df0165dd0022fd5e13b88cd9b64fae8a0dc6924
roman1117459II <roman1117459@gmail.com>
2023-11-20 11:54:58 +0600

    edited done mam

--- 0app.js
+++ 0app.js
@@ -1,2 +1,8 @@
 console.log("Hello JS Starting By Zaima!")
 console.log("Hello JS Starting By Ratul!")
+var gg= ["zaima", "ratul", "rtza"]
+
+for(var i= 0; i<gg.length; i++){
+
+	console.log(gg[i]);
+}
